0fc1a72eb666bc6c32e71153528c7699cc4a9987
soytw <yo@soytw.noip.me>
2017-01-18 23:05:02 +0800

    checker

--- index.php
+++ index.php
@@ -1,51 +1,29 @@
 <?php
 require_once("function/cURL-HTTP-function/curl.php");
 require_once("function/TNFSH-Login/Login.php");
-if(isset($_POST['id'])&&checklogin($_POST['id'],$_POST['pwd'])){
-	?>
-		<meta charset="UTF-8">
-		<base href="http://svrsql.tnfsh.tn.edu.tw/webschool/STD_DAY.asp">
-	<?php
+
+$id = "";
+$pwd = "";
+if (checklogin($id, $pwd)){
 	$html=cURL_HTTP_Request("http://svrsql.tnfsh.tn.edu.tw/webschool/STD_DAY.asp",null,false,"cookie.txt")->html;
 	$html=iconv("BIG5", "UTF-8", $html);
 	$html=str_replace("Big5", "UTF-8", $html);
 	$html=str_replace("\r\n", "", $html);
-	preg_match('/^(.+<td class="ColumnTDX"><font class="ColumnFONT">課外活動<\/font><\/a><\/td> <\/tr>).*?(<tr> <td colspan="14" class="ColumnTD">.+)$/', $html, $header);
-	echo $header[1];
 	$html=preg_match_all('/<tr><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td><td class="DataTDX?"><font class="DataFONTX?">.*?&nbsp;<\/font><\/td> <\/tr>/', $html, $match);
+	$absence = false;
 	foreach ($match[0] as $temp) {
-		if(strpos($temp, "曠課")!==false||strpos($temp, "遲到")!==false||strpos($temp, "缺席")!==false){ 
-			echo $temp;
+		if(strpos($temp, "曠課")!==false||strpos($temp, "遲到")!==false||strpos($temp, "缺席")!==false){
+			$absence = true;
 		}
 	}
-	echo $header[2];
-}else {
-	?>
-	<html>
-	<head>
-		<meta charset="UTF-8">
-	</head>
-	<body>
-	<center>
-		<h2>TNFSH列印曠課</h2>
-		<h3>Open Source: <a href="https://github.com/Xi-Plus/TNFSH-Absence-Printer" target="_blank">On Github</a></h3>
-		<form action="" method="post">
-			帳號： <input name="id" type="text"><br>
-			密碼： <input name="pwd" type="password"><br>
-			<input name="" type="submit" value="送出">
-		</form>
-		<?php
-		if(isset($_POST['id'])&&!checklogin($_POST['id'],$_POST['pwd'])){
-			echo "登入失敗";
-		}
-		?>
-	<hr>
-	<?php
-	@include("function/Xiplus-Facebook-Badge/badge.php");
-	?>
-	</center>
-	</body>
-	</html>
-	<?php
+	if ($absence) {
+		echo "There is absence\n";
+		return 1;
+	} else {
+		echo "No absence\n";
+		return 0;
+	}
+} else {
+	echo "Login failed\n";
+	return 100;
 }
-?>
